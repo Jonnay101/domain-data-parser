39eedb58942b6d119917e18b081f50ae793f62cf
John Hughes <john.hughes101@gmail.com>
2021-07-30 08:59:34 +0100

    REMOVE COMMENTED CODE

--- pkg/datapipeline/datapipeline.go
+++ pkg/datapipeline/datapipeline.go
@@ -1,16 +1,6 @@
 package datapipeline
 
-// import "github.com/jonnay101/domain-data-parser/pkg/store"
-
 type DomainStats interface{}
-
-// // GetDomainStats returns a DomainData store. This store maps domain names against
-// // their frequency of occurence in the provided customer data CSV file
-// func GetDomainStats(filepath string) (DomainStats, error) {
-// 	emailChan := parseCSVData(filepath)
-// 	domChan := parseDomainNames(emailChan)
-// 	return saveDomainStats(domChan)
-// }
 
 type DataPipeline interface {
 	Run(filepath string) error
